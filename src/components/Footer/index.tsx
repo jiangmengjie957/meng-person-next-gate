e1c0646e466a23b96eef27a45ebcca7d42a9256c
jiangmengjie <976080119jmj@gmail.com>
2025-11-26 15:57:03 +0800

    fix: 修改地址

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
             <Image width={16} height={16} alt="copyright" src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/img/police.d0289dc.png" />
             <a
                 style={{ color: "#000" }}
-                href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=浙ICP备2024086363号"
+                href="https://beian.miit.gov.cn"
                 target="_blank"
             >
                 浙ICP备2024086363号
